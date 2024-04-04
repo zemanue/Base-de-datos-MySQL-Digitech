a71f54663ae6befaa853d0c86ab39d06b26ab05a
zemanue <manureco.97@gmail.com>
2024-04-04 15:38:45 +0200

    Update Tarea 8 BD MYSQL - Consultas de BD de empleados.sql
    Exercise using DQL finished. Questions:
    1. Calculates the average salary of the employees in department 20 of the EMPLOYEES table.
    2. Counts how many records in the EMPLOYEES table.
    3. Counts how many records in the EMPLOYEES table charge commission.
    4. Calculates the maximum salary of the EMPLOYEES table.
    5. Get the maximum name (alphabetically) of the EMPLOYEES table.
    5. Get the name of the employee with the highest salary.
    6. Get the minimum salary from the EMPLOYEES table.
    7. Get the name of the employee with the minimum salary.
    8. Calculate the sum of all the salaries in the EMPLOYEES table.
    9. Get the standard deviation of all the salaries in the EMPLOYEES table. Find out first what the standard deviation is.
    10. Calculate the number of departments in the EMPLOYEES table. Then, calculate the number of people in each department in the EMPLOYEES table.
    11. From the EMPLOYEES table, visualize how many last names begin with the letter 'A'.
    12. Get the name of the employees starting with the letter 'A' and who have the highest salary (of those starting with the letter 'A').

--- Tarea 8 BD MYSQL - Consultas de BD de empleados.sql
+++ Tarea 8 BD MYSQL - Consultas de BD de empleados.sql
@@ -47,3 +47,48 @@ INSERT INTO EMPLEADOS VALUES (7876,'ALONSO','EMPLEADO',7788,'1981/09/23',143000,
 INSERT INTO EMPLEADOS VALUES (7900,'JIMENO','EMPLEADO',7698,'1981/12/03',123500,NULL,30);
 INSERT INTO EMPLEADOS VALUES (7902,'FERNANDEZ','ANALISTA',7566,'1981/02/03',390000,NULL,20);
 INSERT INTO EMPLEADOS VALUES (7934,'MUROS','EMPLEADO',7782,'1982/01/23',169000,NULL,10);
+
+
+--Deseamos realizar las siguientes consultas:
+
+--1. Calcula el salario medio de los empleados del departamento 20 de la tabla EMPLEADOS.
+SELECT AVG(salario) AS SALARIO_MEDIO_DEPT20 FROM empleados WHERE dept_no=20; 
+
+--2. Cuenta cuántos registros la tabla EMPLEADOS.
+SELECT COUNT(*) FROM empleados;
+
+--3. Cuenta cuántos registros de la tabla EMPLEADOS cobran comisión.
+SELECT COUNT(comision) FROM empleados WHERE comision>0;
+
+--4. Calcula el salario máximo de la tabla EMPLEADOS.
+SELECT MAX(salario) FROM empleados;
+
+--5. Obtén el máximo nombre (alfabéticamente) de la tabla EMPLEADOS.
+SELECT MAX(apellido) FROM empleados;
+
+--6. Obtén el nombre del empleado que tiene mayor salario.
+SELECT apellido FROM empleados WHERE salario = (SELECT MAX(salario) FROM empleados);
+
+--7. Obtén el mínimo salario de la tabla EMPLEADOS.
+SELECT MIN(salario) FROM empleados;
+
+--8. Obtén el nombre del empleado que tiene el mínimo salario.
+SELECT apellido FROM empleados WHERE salario = (SELECT MIN(salario) FROM empleados);
+
+--9. Calcula la suma de todos los salarios de la tabla EMPLEADOS.
+SELECT SUM(salario) FROM empleados;
+
+--10. Obtén la desviación típica de todos los salarios de la tabla EMPLEADOS. Averigua antes que es la desviación típica.
+-- La desviación típica es una medida que sirve para cuantificar la variación de los datos numéricos con respecto a la media.
+SELECT STDDEV_POP(salario) FROM empleados;
+
+--11. Calcula el número de departamentos que hay en la tabla EMPLEADOS. Después,  calcula el número de personas que hay en cada departamento que hay en la tabla EMPLEADOS.
+SELECT COUNT(dept_no) FROM empleados GROUP BY dept_no;
+SELECT dept_no, COUNT(*) FROM empleados GROUP BY dept_no;
+
+--12. A partir de la tabla EMPLEADOS, visualiza cuántos apellidos empiezan por la letra ‘A’.
+SELECT COUNT(apellido) AS APELLIDOS_POR_A FROM empleados WHERE apellido LIKE 'a%';
+
+--13. Obtén el nombre de los empleados que empiecen por la letra ‘A’ y que tengan máximo salario (de los que empiezan por la letra ‘A’).
+SELECT MAX(salario) FROM empleados WHERE apellido LIKE 'a%';
+
