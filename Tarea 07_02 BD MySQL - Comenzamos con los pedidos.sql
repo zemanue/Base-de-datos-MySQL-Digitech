208a3076ba81b8d9b33d4287010821ea150c8d47
zemanue <manureco.97@gmail.com>
2024-04-09 13:11:11 +0200

    Update Tarea 07_02 BD MySQL - Comenzamos con los pedidos.sql
    DML done and code tested. Exercise finished.

--- Tarea 07_02 BD MySQL - Comenzamos con los pedidos.sql
+++ Tarea 07_02 BD MySQL - Comenzamos con los pedidos.sql
@@ -38,4 +38,24 @@ FOREIGN KEY (RefPed) REFERENCES Pedido (RefPed),
 ADD CONSTRAINT fk_LineaPedido_Articulo
 FOREIGN KEY (CodArt) REFERENCES Articulo (CodArt);
 
+INSERT INTO Pedido VALUES
+('P0001', '2014-02-16'),
+('P0002', '2014-02-18'),
+('P0003', '2014-02-23'),
+('P0004', '2014-02-25');
 
+INSERT INTO Articulo VALUES
+('A0043', 'Boligrafo azul fino', 0.78),
+('A0078', 'Boligrafo rojo normal', 1.05),
+('A0075', 'Lapiz 2B', 0.55),
+('A0012', 'Goma de borrar', 0.15),
+('A0089', 'Sacapuntas', 0.25);
+
+INSERT INTO LineaPedido VALUES
+('P0001', 'A0043', 10),
+('P0001', 'A0078', 12),
+('P0002', 'A0043', 5),
+('P0003', 'A0075', 20),
+('P0004', 'A0012', 15),
+('P0004', 'A0043', 5),
+('P0004', 'A0089', 50);
